54ddfd11fdc965cc4e50f5772204b8b07a0f978e
Blake Collins <blakeac369@gmail.com>
2020-07-30 16:46:19 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,79 @@
+const main = document.querySelector('main'),
+      vociesSelect = document.getElementById('voices'),
+      textarea = document.getElementById('text'),
+      readBtn = document.getElementById('read'),
+      toggleBtn = document.getElementById('toggle'),
+      closeBtn = document.getElementById('close');
+
+
+
+      const data = [
+        {
+          image: './img/drink.jpg',
+          text: "I'm Thirsty"
+        },
+        {
+          image: './img/food.jpg',
+          text: "I'm Hungry"
+        },
+        {
+          image: './img/tired.jpg',
+          text: "I'm Tired"
+        },
+        {
+          image: './img/hurt.jpg',
+          text: "I'm Hurt"
+        },
+        {
+          image: './img/happy.jpg',
+          text: "I'm Happy"
+        },
+        {
+          image: './img/angry.jpg',
+          text: "I'm Angry"
+        },
+        {
+          image: './img/sad.jpg',
+          text: "I'm Sad"
+        },
+        {
+          image: './img/scared.jpg',
+          text: "I'm Scared"
+        },
+        {
+          image: './img/outside.jpg',
+          text: 'I Want To Go Outside'
+        },
+        {
+          image: './img/home.jpg',
+          text: 'I Want To Go Home'
+        },
+        {
+          image: './img/school.jpg',
+          text: 'I Want To Go To School'
+        },
+        {
+          image: './img/grandma.jpg',
+          text: 'I Want To Go To Grandmas'
+        }
+      ];
+
+data.forEach(createBox);
+
+// Create speech boxes
+function createBox(item) {
+  const box = document.createElement('div');
+
+  const { image, text } = item;
+
+  box.classList.add('box');
+
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+  
+
+  main.appendChild(box);
+}
