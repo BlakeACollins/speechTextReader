20a6154ad7c986a007485a002f9f3d2e7b2afc42
Blake Collins <blakeac369@gmail.com>
2020-07-31 16:18:05 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,62 +1,60 @@
 const main = document.querySelector('main'),
-      vociesSelect = document.getElementById('voices'),
+      voicesSelect = document.getElementById('voices'),
       textarea = document.getElementById('text'),
       readBtn = document.getElementById('read'),
       toggleBtn = document.getElementById('toggle'),
       closeBtn = document.getElementById('close');
 
-
-
-      const data = [
-        {
-          image: './img/drink.jpg',
-          text: "I'm Thirsty"
-        },
-        {
-          image: './img/food.jpg',
-          text: "I'm Hungry"
-        },
-        {
-          image: './img/tired.jpg',
-          text: "I'm Tired"
-        },
-        {
-          image: './img/hurt.jpg',
-          text: "I'm Hurt"
-        },
-        {
-          image: './img/happy.jpg',
-          text: "I'm Happy"
-        },
-        {
-          image: './img/angry.jpg',
-          text: "I'm Angry"
-        },
-        {
-          image: './img/sad.jpg',
-          text: "I'm Sad"
-        },
-        {
-          image: './img/scared.jpg',
-          text: "I'm Scared"
-        },
-        {
-          image: './img/outside.jpg',
-          text: 'I Want To Go Outside'
-        },
-        {
-          image: './img/home.jpg',
-          text: 'I Want To Go Home'
-        },
-        {
-          image: './img/school.jpg',
-          text: 'I Want To Go To School'
-        },
-        {
-          image: './img/grandma.jpg',
-          text: 'I Want To Go To Grandmas'
-        }
-      ];
+const data = [
+  {
+    image: './img/drink.jpg',
+    text: "I'm Thirsty"
+  },
+  {
+    image: './img/food.jpg',
+    text: "I'm Hungry"
+  },
+  {
+    image: './img/tired.jpg',
+    text: "I'm Tired"
+  },
+  {
+    image: './img/hurt.jpg',
+    text: "I'm Hurt"
+  },
+  {
+    image: './img/happy.jpg',
+    text: "I'm Happy"
+  },
+  {
+    image: './img/angry.jpg',
+    text: "I'm Angry"
+  },
+  {
+    image: './img/sad.jpg',
+    text: "I'm Sad"
+  },
+  {
+    image: './img/scared.jpg',
+    text: "I'm Scared"
+  },
+  {
+    image: './img/outside.jpg',
+    text: 'I Want To Go Outside'
+  },
+  {
+    image: './img/home.jpg',
+    text: 'I Want To Go Home'
+  },
+  {
+    image: './img/school.jpg',
+    text: 'I Want To Go To School'
+  },
+  {
+    image: './img/grandma.jpg',
+    text: 'I Want To Go To Grandmas'
+  }
+];
 
 data.forEach(createBox);
 
@@ -73,7 +71,72 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
-  
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add active effect
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  });
 
   main.appendChild(box);
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// Set voice
+function setVoice(e) {
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show')
+);
+
+// Close button
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show')
+);
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// Read text button
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
